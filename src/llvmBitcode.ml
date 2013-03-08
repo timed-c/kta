028430db15944ca9122ab87f116ced45a1de26fd
David Broman <david.broman@liu.se>
2013-03-07 20:16:17 -0800

    Fixed width decoding of integer supported.

--- src/llvmBitcode.ml
+++ src/llvmBitcode.ml
@@ -20,12 +20,18 @@ type bitstream = BS of string * strpos * decoded * decoded_bits
 
 (*************** Local functions **********************************)
 
+let intsize = Sys.word_size - 1
+
 (* Functions raising various of decoding errors. *)
 let error_header() = raise (Bitcode_error "Illegal LLVM bitcode header.")
 let error_align() = raise (Bitcode_error "Illegal alignment in the LLVM bitcode data.")
 let error_size() = raise (Bitcode_error "Illegal size of LLVM bitcode data.")
 let error_version v = raise (Bitcode_error (sprintf "LLVM Bitcode version %d is not supported." v))
-
+let error_intsize() = raise (Bitcode_error 
+         (sprintf "The machine's word size of %d bits is not large enough." Sys.word_size))
+                                                   
+(* Return an integer with [n] bits set *)
+let ones n = (1 lsl n) - 1 
 
 (* Get an int value out of a string array *)
 let s2int s i = int_of_char (s.[i])
@@ -46,7 +52,6 @@ let dBytes (BS(s,p,d,b)) n =
   check_alignment b;
   check_byte_size s p n;
   (String.sub s p n, BS(s,p+n,0,0))
-
   
 (* Decode a 32-bits word in little-endian format*)
 let dWord32 (BS(s,p,d,b)) =
@@ -56,8 +61,20 @@ let dWord32 (BS(s,p,d,b)) =
           ((s2int s (p+2)) lsl 16) lor  ((s2int s (p+3)) lsl 24) in
   (v, BS(s,p+4,0,0))
 
+(* Decode a Fixed width Integer of bit size [n]. *)
+let dFI (BS(s,p,d,b)) n = 
+  let rec decode p d b =
+    if b > intsize then error_intsize() 
+    else if b >= n then 
+      let v =  d land (ones n) in
+      (v, BS(s,p,d lsr n, b-n))
+    else(
+      check_byte_size s p 1;
+      let d' = ((s2int s p) lsl b) lor d in
+      decode (p + 1) d' (b+8))
+  in decode p d b
+      
   
-
 (** Decodes the bitcode header (if present) as well as the
     bitstream magic number. All data is checked for consistency *)
 let decode_header bitstr =
@@ -89,8 +106,15 @@ let decode_header bitstr =
 
 let decode data =
   let bs = BS(data,0,0,0) in
-  let BS(s,p,d,b) = decode_header bs in
-  printf "String length length=%d, end_pos=%d (%x)\n" (String.length data) p p
+  let bs = decode_header bs in
+  let BS(s,p,d,b) = bs in
+  printf "String length length=%d, end_pos=%d (%x)\n" (String.length data) p p;
+  let (v1,bs) = dFI bs 2 in
+  let (v2,bs) = dFI bs 8 in
+  let (v3,bs) = dFI bs 4 in
+  let (v4,bs) = dFI bs 18 in
+  let (v5,bs) = dFI bs 8 in
+  printf "Decoded %d,%d,%d,%d,%d\n" v1 v2 v3 v4 v5
 
 
 
