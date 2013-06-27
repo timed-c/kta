818dbc82bdd170e24fed62e7f484382d1d0883f7
David Broman <broman@eecs.berkeley.edu>
2013-06-27 10:21:53 -0700

    Added eval functions (not finished).

--- src/ptllvm/llvmEval.ml
+++ src/ptllvm/llvmEval.ml
@@ -16,6 +16,9 @@ type btime = llabel -> llabel -> time
 
 (*************** Local types and exceptions ***********************)
 
+type instTy = 
+| InstRet    of llConst option 
+| InstBranch of llabel * (llId,llConst) PMap.t
 
 
 (*************** Local functions **********************************)
@@ -35,16 +38,16 @@ let eval_expr env e =
   | VConst(c) -> c
   | VConstExpr(_) -> CInt(1,Int64.zero)   (* TODO *)
 
-let eval_bop bop v1 v2 =
-  match v1,v2 with
+let eval_bop bop val1 val2 =
+  match val1,val2 with
   | CInt(w,v1),CInt(_,v2) -> (
     let andval = Int64.of_int ((1 lsl w)-1) in
-    Int64.logand andval (
+    CInt(w,Int64.logand andval (
       match bop with     
       | BopAdd -> Int64.add v1 v2
       | BopSub -> Int64.sub v1 v2
       | BopMul -> Int64.mul v1 v2
-      | BopUDiv | BopSDiv | BopURem | BopSRem -> failwith "todo"
+      | BopUDiv | BopSDiv | BopURem | BopSRem -> failwith "TODO bop"
       | BopShl -> Int64.shift_left v1 (Int64.to_int v2)
       | BopLShr -> Int64.shift_right_logical v1 (Int64.to_int v2)
       | BopAShr -> Int64.shift_right v1 (Int64.to_int v2)
@@ -52,25 +55,56 @@ let eval_bop bop v1 v2 =
       | BopOr -> Int64.logor v1 v2
       | BopXor -> Int64.logxor v1 v2
       | BopFAdd | BopFSub | BopFMul | BopFDiv | BopFRem 
-          -> raise Illegal_llvm_code))
+          -> raise Illegal_llvm_code)))
   | _ -> raise Illegal_llvm_code
 
   
-(*
-let rec eval_inst instlst time env timeout =
+let eval_icmp_pred icmppred val1 val2 =
+  match val1,val2 with
+  | CInt(w,v1), CInt(_,v2) -> (
+    CInt(1, (
+      match icmppred with
+      | IcmpEq  -> if Int64.compare v1 v2 == 0 then Int64.one else Int64.zero
+      | IcmpNe  -> if Int64.compare v1 v2 != 0 then Int64.one else Int64.zero
+      | IcmpUgt -> if Int64.compare v1 v2 >  0 then Int64.one else Int64.zero
+      | IcmpUge -> if Int64.compare v1 v2 >= 0 then Int64.one else Int64.zero
+      | IcmpUlt -> if Int64.compare v1 v2 <  0 then Int64.one else Int64.zero
+      | IcmpUle -> if Int64.compare v1 v2 <= 0 then Int64.one else Int64.zero
+      | IcmpSgt | IcmpSge | IcmpSlt | IcmpSle ->
+          failwith "Signed comparison not yet implemented")))
+  | _ -> raise Illegal_llvm_code
+
+
+(* Evaluate a list of instructions *)
+let rec eval_inst instlst env  =
   match instlst with
+  | IRet(None)::[] -> InstRet(None)
+  | IRet(Some(_,e))::[] -> InstRet(Some(eval_expr env e)) 
+  | IBrCond(exp,l1,l2)::[] -> (
+    match eval_expr env exp with
+    | CInt(1,v) when Int64.to_int v = 1 -> InstBranch(l1,env)
+    | CInt(1,v) when Int64.to_int v = 0 -> InstBranch(l2,env)
+    | _ -> raise Illegal_llvm_code)
+  | IBrUncond(l)::[] -> InstBranch(l,env)
   | IBinOp(id,bop,ty,v1,v2)::lst -> 
-    LocalId(eval_bop bop (eval_expr env v1) (eval_expr env v2))
+    let nval = eval_bop bop (eval_expr env v1) (eval_expr env v2) in
+    let env' = env_add (LocalId(id)) nval env in
+    eval_inst lst env'  
+  | ICmp(id,pred,ty,v1,v2)::lst ->
+    let nval = eval_icmp_pred pred (eval_expr env v1) (eval_expr env v2) in
+    let env' = env_add (LocalId(id)) nval env in
+    eval_inst lst env'  
+  | _ -> failwith "Instruction not implemented."
 
- of      (* Various binary operations. See llBinOp *)
-    llLocId *         (* Assignment id *) 
-    llBinOp *         (* Binop *)
-    llType *          (* Operand type *)        
-    llVal *           (* Op1 *)
-    llVal             (* Op2 *)
-    
+(*
+(* Evaluate phi functions *)
+let eval_phi phis src_label env =
+  let foldfun env (LLPhi(id,ty,
+  List.fold_left (fun 
 *)
+  
 
+  
 
 (*
 let eval_block instlst src_label block_label env timeout =  
@@ -82,7 +116,7 @@ let eval_function blocks params args env timeout =
   let start = match blocks with b::_ -> b | _ -> raise Illegal_llvm_code in  
   (* Augment environment with arguments *)
   let parargs = try List.combine params args with _ -> raise Illegal_llvm_code in
-  let env = List.fold_left (fun e ((ty,id), v) -> env_add id v e) env parargs in
+  let env = List.fold_left (fun e ((ty,id), v) -> env_add id v e) env parargs in 
   
   (1, VConst(CInt(1,Int64.zero)))
 
