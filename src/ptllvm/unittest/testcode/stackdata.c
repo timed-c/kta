31c4b61972811a89f439282b4487a4082b695b1f
David Broman <broman@eecs.berkeley.edu>
2013-07-05 13:28:55 -0700

    Added matrix stack examples.

--- src/ptllvm/unittest/testcode/stackdata.c
+++ src/ptllvm/unittest/testcode/stackdata.c
@@ -24,11 +24,37 @@ int simple_array_access(int k, int n)
   return a[n];
 }
 
+int simple_matrix_access(int x, int y, int x2, int y2, int k)
+{
+  int m[8][12];
+  m[x][y] = k;
+  return m[x2][y2];
+}
+
+
+int idx(int* v, int n)
+{
+  return v[n];
+}
+
+int simple_matrix_access2(int x, int y, int x2, int y2, int k)
+{
+  int m[8][12];
+  int* p;
+  int s;
+  m[x][y] = k;
+  m[x][y+1] = k;
+  p = m[x2];
+  s = p[y2] + idx(p,y2+1);
+  return s;
+}
 
 
 int main()
 {
   printf("addnums(7) = %d\n", addnums(7));
   printf("simple_array_access(3,3) = %d\n", simple_array_access(3,3));
+  printf("simple_matrix_access(4,5,4,5,10) = %d\n", simple_matrix_access(4,5,4,5,10));
+  printf("simple_matrix_access2(4,5,4,5,10) = %d\n", simple_matrix_access2(4,4,4,4,10));
   return 0;
 }
