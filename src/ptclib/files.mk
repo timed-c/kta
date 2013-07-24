dad8d48f1df61dfc77b3193f70ff1b8713a9374c
David Broman <broman@eecs.berkeley.edu>
2013-07-24 05:14:47 -0700

    Added dummy test for ptclib.

--- src/ptclib/files.mk
+++ src/ptclib/files.mk
@@ -1,8 +1,8 @@
 
 LIB_NAME = ptclib
-FILES =  ssacfg
-TEST_FILES = testSSACFG
+FILES =  ssaCfg
+TEST_FILES = testSsaCfg
 ONLY_MLI_FILES = 
 INCLUDE_DIR = ../utools/ ../ptllvm/
-DEP_LIBS = utools.cmxa ptllvm.cmx
+DEP_LIBS = utools.cmxa ptllvm.cmxa
 UNITTEST_DIR = utest
---
+++ src/ptclib/utest/testSsaCfg.ml
@@ -0,0 +1,17 @@
+
+
+open Ustring.Op
+open Utest
+
+
+let main = 
+
+  init "Test operations over SSACFGs";
+
+
+
+  test_int "Dummy test" (SsaCfg.construct 0) 0;
+
+    
+  result()
+
