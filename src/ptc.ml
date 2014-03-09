32b7d7a9f289e64ac485998a17319fdada2cfa11
David Broman <broman@eecs.berkeley.edu>
2014-03-08 20:02:34 -0800

    Added dummy analysis function.

--- src/ptc.ml
+++ src/ptc.ml
@@ -10,48 +10,58 @@ type  arg_error_type =
 
 exception Argument_error
 
-type files_record = {
+(* Structure representing the sorted list of different file arguments 
+   that were supplied when executing ptc. *)
+type filename_args = {
   c : ustring list;
   ll: ustring list;
 }
 
 
-
-let fold_list lst f = 
-  List.rev (List.fold_left f [] lst)
-
 type filename = ustring
-type lineno = int
-type argno = int
-type varid = sid
+type lineno = int            (* Line number *)
+type argno = int             (* Argument number. Used for assumptions of arguments. *)
 
+(* Representing execution time *)
+type time = 
+| TimeCycles of int     (* Time in clock cycles *)
+| TimeInfinity    (* The time is proven to be infinite *)
+| TimeUnknown     (* A safe bound can not be computed. This does not, however,
+                     mean that such a bound does not exist *)
+ 
 (* Timing Program Point *)
 type tpp = 
-|TppEnty   
+|TppEntry   
 |TppExit 
 |TppNode of int
 
-
-
+(* Abstract value *)
 type value = 
 | VInt of int * int (* Upper and lower bounds on integers *)
 
+(* Different forms of timing requests *)
 type ta_req = 
-| ReqWCP of tpp * tpp
-| ReqBCP of tpp * tpp
-| ReqLWCET of tpp * tpp
-| ReqLBCET of tpp * tpp
-| ReqFWCET of tpp * tpp
-| ReqFBCET of tpp * tpp
+| ReqWCP of tpp * tpp   (* Worst-case path request *)
+| ReqBCP of tpp * tpp   (* Best-case path request *)
+| ReqLWCET of tpp * tpp (* Local worst-case execution time request *)
+| ReqLBCET of tpp * tpp (* Local best-case execution time request *)
+| ReqFWCET of tpp * tpp (* Fractional worst-case execution time request *)
+| ReqFBCET of tpp * tpp (* Fractional best-case execution time request *)
 
+(* Different possible responses to a path request *)
+type tpp_path =
+| TppPath of tpp list   (* Path represented as a list of timing program points *)
+| TppPathInfinity       (* The path is proven to be infinite *)
+| TppPathUnknown        (* The path is unknown. Could not be computed *)
+
+(* Different form of timing analysis responses *)
 type ta_res = 
-| ReqWCP of tpp list
-| ReqBCP of tpp list
-| ReqLWCET of int
-| ReqLBCET of int
-| ReqFWCET of int
-| ReqFBCET of int
-
+| ResWCP of tpp_path  
+| ResBCP of tpp_path
+| ResLWCET of time
+| ResLBCET of time
+| ResFWCET of time
+| ResFBCET of time
 
 (* Structure to represent a timing analysis request for a specific function *)
 type func_ta_req = {
@@ -60,7 +70,7 @@ type func_ta_req = {
   gvars : (sid * value) list;       (* Global variable assumptions *)
   fwcet : (sid * int) list;         (* Assumed WCET in clock cycles for functions *)
   fbcet : (sid * int) list;         (* Assumed BCET in clock cycles for functions *)  
-  reqta : (lineno * ta_req) list;   (* Requested timing analysis values *)
+  ta_req : (lineno * ta_req) list;  (* Requested timing analysis values *)
 }
 
 (* Timing analysis requests within a file *)
@@ -70,19 +80,78 @@ type file_ta_req = {
   ta_requests : func_ta_req list;    (* List of ta requests *)
 }
 
+(* Returns the pair of timing program points from a timing request *)
+let get_req_tpp req = 
+  match req with ReqWCP(t1,t2)  | ReqBCP(t1,t2) | ReqLWCET(t1,t2) | ReqLBCET(t1,t2) |
+                 ReqFWCET(t1,t2) | ReqFBCET(t1,t2) -> (t1,t2)
 
+(* Compare two tpp values. Compatible with the standard 'compare' interface. *)
+let tpp_compare t1 t2 = 
+  match t1,t2 with
+  | TppEntry,TppEntry -> 0  
+  | TppEntry,_ -> -1
+  | _,TppEntry -> 1
+  | TppExit,TppExit -> 0
+  | TppExit,_ -> 1
+  | _,TppExit -> -1
+  | TppNode(v1),TppNode(v2) -> compare v1 v2
   
-  
+(* Function [list_remove_duplicates f l] takes a list [l], sorts it using comparsion
+   function [f] and removes any duplicates that exists in the list. The returned
+   list is in reversed sorted order without any duplicates *)
+let list_remove_duplicates compare_func list = 
+  let rec work list acc = 
+    match list with
+    | x1::x2::ls -> if compare_func x1 x2 = 0 then work (x2::ls) acc 
+                    else work (x2::ls) (x1::acc)
+    | [x1] -> x1::acc
+    | [] -> acc
+  in
+    work (List.fast_sort compare_func list) []
+    
+
 (* Takes a timing analysis function request and returns a list of timing analysis
    responses *)
-let dummy_timing_analysis func_ta_req = []
+let dummy_timing_analysis func_ta_req = 
+  (* Get all programming point numbers *)
+  let tpps = List.fold_left (fun acc (_,tareq) -> 
+    let (t1,t2) = get_req_tpp tareq in t1::t2::acc 
+  ) [] (func_ta_req.ta_req)
+  in
   
+  (* Remove all duplicates *)
+  let tpps_distinct = list_remove_duplicates tpp_compare tpps in
 
-let make_full_ta_res_file_string file_ta_req = []
+  (* Sort and create absolute time for each tpp as an associate list *)
+  let timediff = 100 in
+  let tpp_times = snd (List.fold_left (fun (time,acc) tpp ->
+    (time + timediff, (tpp,time)::acc)    
+  ) (timediff,[]) tpps_distinct)
+  in
   
-  
+  (* Generate the response *)
+  List.rev (List.fold_left (fun acc (_,tareq) -> 
+    let find_path t1 t2 = (
+      let cv = tpp_compare t1 t2 in
+      if cv = 0 then TppPath([t1]) else if cv > 0 then TppPathInfinity else 
+          TppPath(List.filter (fun x -> 
+            tpp_compare x t1 >= 0 && tpp_compare x t2 <= 0) tpps_distinct))
+    in
+    let get_ta_time t1 t2 wrong_order =
+      let cv = tpp_compare t1 t2 in
+      if cv = 0 then TimeCycles(0) else if cv > 0 then wrong_order else 
+      TimeCycles(List.assoc t2 tpp_times - List.assoc t1 tpp_times)
+    in                                                               
+    match tareq with
+    | ReqWCP(t1,t2)  -> ResWCP(find_path t1 t2)::acc
+    | ReqBCP(t1,t2) ->  ResBCP(find_path t1 t2)::acc
+    | ReqLWCET(t1,t2) -> ResLWCET(get_ta_time t1 t2 TimeInfinity)::acc
+    | ReqLBCET(t1,t2) -> ResLBCET(get_ta_time t1 t2 TimeInfinity)::acc
+    | ReqFWCET(t1,t2) -> ResFWCET(get_ta_time t1 t2 (TimeCycles(0)))::acc
+    | ReqFBCET(t1,t2) -> ResFBCET(get_ta_time t1 t2 (TimeCycles(0)))::acc
+  ) [] func_ta_req.ta_req)
 
-  
+                      
 
 
 
@@ -106,13 +175,13 @@ let parse_ta_strings lines =
   let utf8tokens = List.map (fun (n,ls) -> (n,List.map Ustring.to_utf8 ls)) tokenlst in 
   
   (* Extract timing analysis request data *)
-  let rec extract tokens fname args gvars fwcet fbcet reqta acc =
+  let rec extract tokens fname args gvars fwcet fbcet ta_req acc =
     match tokens with
     | (line_no,["function";name])::ts -> (
          match fname with
          | Some(prename) -> 
            (* We are done with this function. Process next *)
-           let new_functa =  {funcname = us prename; args; gvars; fwcet; fbcet; reqta} in
+           let new_functa =  {funcname = us prename; args; gvars; fwcet; fbcet; ta_req} in
            extract ts (Some(name)) [] [] [] [] [] (new_functa::acc)
          | None -> extract ts (Some(name)) [] [] [] [] [] acc)
     | _ -> acc
@@ -137,6 +206,8 @@ let parse_ta_file filename =
   (* Parse requested string and return the request structure for a file *)
   let tareqs = parse_ta_strings lines in
   {filename; lines; ta_requests = tareqs} 
+
+
 
 
 
