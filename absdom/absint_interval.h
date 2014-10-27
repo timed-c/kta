e39a80ce9b8f4a10509634b1ab54458712d22f14
David Broman <broman@eecs.berkeley.edu>
2014-10-27 09:40:51 +0100

    Updated names in absint

--- absdom/absint_interval.h
+++ absdom/absint_interval.h
@@ -16,13 +16,13 @@
 #define sign_extend(x,w) ((((int)(x)) << (intbits - (w))) >> (intbits - (w)))
 
 /* Set a variable to an interval. w is the word bith length  */
-#define aint_set_interval(name,l,h,w) \
+#define set_interval(name,l,h,w) \
   name##low = l & mask(w); \
   name##high = h & mask(w)
 
 /* Adds x and y and stores into z. Assumes that 
    all abstract values have w bit width */
-#define aint_add(z,x,y,w) \
+#define add(z,x,y,w) \
   z##high = (x##high + y##high) & mask(w);\
   z##low = (x##low + y##low) & mask(w);\
   if(x##low > x##high && z##high < x##high ||\
@@ -36,7 +36,7 @@
 /* Multiplies x and y and stores it in z. 
    Must check how good this works for negative numbers. 
    This abstracted instruction can be improved.*/
-#define aint_mul(z,x,y,w)\
+#define mul(z,x,y,w)\
   if(!(x##low == x##high && y##low == y##high) &&\
     (x##high > maxval(w/2) || y##high > maxval(w/2) || \
      x##low > x##high || y##low > y##high)) \
@@ -51,7 +51,7 @@
   }
 
 
-#define aint_icmp_sgt(z,x,y,w)\
+#define icmp_sgt(z,x,y,w)\
 if((sign_extend(x##high) >= sign_extend(x##low) &&\
     (sign_extend(y##high) >= sign_extend(y##low))\
   {\
@@ -80,11 +80,11 @@ if((sign_extend(x##high) >= sign_extend(x##low) &&\
    
 
 
-#define aint_get_low(name) name##low
-#define aint_get_high(name) name##high
+#define get_low(name) name##low
+#define get_high(name) name##high
 
 
-#define test_aint(desc,name,exp_low,exp_high)           \
+#define test_interval(desc,name,exp_low,exp_high)           \
   if(name##low != exp_low || name##high != exp_high){   \
     printf("ERROR! Incorrect value for test '%s' Current: [%u,%u] " \
            "Expected: [%u,%u]\n"\
--- absdom/test_absint_interval.c
+++ absdom/test_absint_interval.c
@@ -10,95 +10,95 @@ void test_add_instr()
   aint32(y);
   aint32(z);
     
-  aint_set_interval(x,3,100,32);
-  aint_set_interval(y,10,200,32);
-  aint_add(z,x,y,32);
-  test_aint("add: positive values (32-bit)",z,13,300);
+  set_interval(x,3,100,32);
+  set_interval(y,10,200,32);
+  add(z,x,y,32);
+  test_interval("add: positive values (32-bit)",z,13,300);
 
-  aint_set_interval(x,3,20,32);
-  aint_set_interval(y,-10,40,32);
-  aint_add(z,x,y,32);
-  test_aint("add: negative add lower (32-bit)",z,-7,60);
+  set_interval(x,3,20,32);
+  set_interval(y,-10,40,32);
+  add(z,x,y,32);
+  test_interval("add: negative add lower (32-bit)",z,-7,60);
 
-  aint_set_interval(x,3,25,32);
-  aint_set_interval(y,-50,-10,32);
-  aint_add(z,x,y,32);
-  test_aint("add: negative add lower and high (32-bit)",z,-47,15);
+  set_interval(x,3,25,32);
+  set_interval(y,-50,-10,32);
+  add(z,x,y,32);
+  test_interval("add: negative add lower and high (32-bit)",z,-47,15);
 
-  aint_set_interval(x,-30,-5,32);
-  aint_set_interval(y,-50,-10,32);
-  aint_add(z,x,y,32);
-  test_aint("add: lower and high from neg val (32-bit)",z,-80,-15);
+  set_interval(x,-30,-5,32);
+  set_interval(y,-50,-10,32);
+  add(z,x,y,32);
+  test_interval("add: lower and high from neg val (32-bit)",z,-80,-15);
   
-  aint_set_interval(x,30,100,8);
-  aint_set_interval(y,50,156,8);
-  aint_add(z,x,y,8);
-  test_aint("add: max val goes over max (8-bit)",z,80,0);
+  set_interval(x,30,100,8);
+  set_interval(y,50,156,8);
+  add(z,x,y,8);
+  test_interval("add: max val goes over max (8-bit)",z,80,0);
 
-  aint_set_interval(x,2,3,3);
-  aint_set_interval(y,1,2,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits ord sum with ord (case 1)",z,3,5);
+  set_interval(x,2,3,3);
+  set_interval(y,1,2,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits ord sum with ord (case 1)",z,3,5);
 
-  aint_set_interval(x,5,6,3);
-  aint_set_interval(y,1,3,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits ord sum with ord (case 2)",z,6,1);
+  set_interval(x,5,6,3);
+  set_interval(y,1,3,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits ord sum with ord (case 2)",z,6,1);
 
-  aint_set_interval(x,7,7,3);
-  aint_set_interval(y,7,7,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits ord sum with ord (case 3)",z,6,6);
+  set_interval(x,7,7,3);
+  set_interval(y,7,7,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits ord sum with ord (case 3)",z,6,6);
 
-  aint_set_interval(x,0,7,3);
-  aint_set_interval(y,7,7,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits ord sum with ord (case 4)",z,7,6);
+  set_interval(x,0,7,3);
+  set_interval(y,7,7,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits ord sum with ord (case 4)",z,7,6);
 
-  aint_set_interval(x,1,3,3);
-  aint_set_interval(y,6,1,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits ord sum with swap (case 1)",z,7,4);
+  set_interval(x,1,3,3);
+  set_interval(y,6,1,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits ord sum with swap (case 1)",z,7,4);
 
-  aint_set_interval(x,7,7,3);
-  aint_set_interval(y,6,1,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits ord sum with swap (case 2)",z,0,7);
+  set_interval(x,7,7,3);
+  set_interval(y,6,1,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits ord sum with swap (case 2)",z,0,7);
 
-  aint_set_interval(x,1,7,3);
-  aint_set_interval(y,6,1,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits ord sum with swap (case 3)",z,0,7);
+  set_interval(x,1,7,3);
+  set_interval(y,6,1,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits ord sum with swap (case 3)",z,0,7);
 
-  aint_set_interval(x,3,6,3);
-  aint_set_interval(y,6,1,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits ord sum with swap (case 4)",z,1,7);
+  set_interval(x,3,6,3);
+  set_interval(y,6,1,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits ord sum with swap (case 4)",z,1,7);
 
-  aint_set_interval(x,6,1,3);
-  aint_set_interval(y,1,3,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits swap sum with ord (case 1)",z,7,4);
+  set_interval(x,6,1,3);
+  set_interval(y,1,3,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits swap sum with ord (case 1)",z,7,4);
 
-  aint_set_interval(x,6,1,3);
-  aint_set_interval(y,1,7,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits swap sum with ord (case 2)",z,0,7);
+  set_interval(x,6,1,3);
+  set_interval(y,1,7,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits swap sum with ord (case 2)",z,0,7);
 
-  aint_set_interval(x,6,1,3);
-  aint_set_interval(y,3,6,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits swap sum with ord (case 3)",z,1,7);
+  set_interval(x,6,1,3);
+  set_interval(y,3,6,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits swap sum with ord (case 3)",z,1,7);
 
-  aint_set_interval(x,2,1,3);
-  aint_set_interval(y,3,2,3);
-  aint_add(z,x,y,3);
-  test_aint("add: 3-bits swap sum with swap",z,0,7);
+  set_interval(x,2,1,3);
+  set_interval(y,3,2,3);
+  add(z,x,y,3);
+  test_interval("add: 3-bits swap sum with swap",z,0,7);
 
-  aint_set_interval(x,-20,-10,32);
-  aint_set_interval(y,20,100,32);
-  aint_add(z,x,y,32);
-  test_aint("add: in and max vals go over max for 32 bit width",z,0,90);
+  set_interval(x,-20,-10,32);
+  set_interval(y,20,100,32);
+  add(z,x,y,32);
+  test_interval("add: in and max vals go over max for 32 bit width",z,0,90);
   
 }
 
@@ -108,25 +108,25 @@ void test_mul_instr()
   aint32(y);
   aint32(z);
 
-  aint_set_interval(x,20,40,32);
-  aint_set_interval(y,10,100,32);
-  aint_mul(z,x,y,32);
-  test_aint("mul: legal positive values",z,200,4000);  
+  set_interval(x,20,40,32);
+  set_interval(y,10,100,32);
+  mul(z,x,y,32);
+  test_interval("mul: legal positive values",z,200,4000);  
 
-  aint_set_interval(x,20,40,32);
-  aint_set_interval(y,10,0xffffff,32);
-  aint_mul(z,x,y,32);
-  test_aint("mul: too large values",z,0,0xffffffff);  
+  set_interval(x,20,40,32);
+  set_interval(y,10,0xffffff,32);
+  mul(z,x,y,32);
+  test_interval("mul: too large values",z,0,0xffffffff);  
 
-  aint_set_interval(x,20,40,32);
-  aint_set_interval(y,-2,-2,32);
-  aint_mul(z,x,y,32);
-  test_aint("mul: negative number with interval",z,0,0xffffffff);  
+  set_interval(x,20,40,32);
+  set_interval(y,-2,-2,32);
+  mul(z,x,y,32);
+  test_interval("mul: negative number with interval",z,0,0xffffffff);  
 
-  aint_set_interval(x,40,40,32);
-  aint_set_interval(y,-2,-2,32);
-  aint_mul(z,x,y,32);
-  test_aint("mul: negative without interval",z,-80,-80);  
+  set_interval(x,40,40,32);
+  set_interval(y,-2,-2,32);
+  mul(z,x,y,32);
+  test_interval("mul: negative without interval",z,-80,-80);  
 }
 
 
