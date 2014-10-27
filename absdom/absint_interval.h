cccf2add9136447cfa2febbb0c958d26d98e503e
David Broman <broman@eecs.berkeley.edu>
2014-10-27 19:07:50 +0100

    Fixed a new version of abstract add.

--- absdom/absint_interval.h
+++ absdom/absint_interval.h
@@ -6,50 +6,93 @@
 #ifndef _ABS_INTERVAL_DOM
 #define _ABS_INTERVAL_DOM
 
+/*
+  The following library implements a very simple integer interval domain on
+  fixed size integers. 
+
+  For each instruction, the user specifies the bitwith of the operation. All integers
+  are overapproximated to be assumed to represent signed integers. If an operation
+  results in signed overflow, the result will be TOP. 
+
+  Each value in the domain are represented by two values: high and low. 
+  If high < low, then the value is a TOP element. We ususally use the interval
+  [777,-777] to indicate this (just to make it easy to spot when we print the numbers).
+
+  The max and min values with witdth w are 
+   maxval = (2 << (w-1)) - 1
+   minval = (2 << (w-1)) * -1
+
+  Example 3-bit value. Signed numbers are represented using two's complement
+
+  Bin  Unsigned   Signed
+  000  0          0
+  001  1          1
+  010  2          2
+  011  3          3
+  100  4         -4
+  101  5         -3
+  110  6         -2
+  111  7         -1
+
+  00   0          0
+  01   1          1
+  10   2         -2
+  11   3         -1
+
+  0    0          0
+  1    1         -1
+
+  In this case, MAX = 3 and MIN = -4
+
+  In the following, the semantics of each operation is described briefly
+  
+  ADD: z = x + y
+   - If x = TOP or y = TOP then z = TOP 
+   - Let th = x.high + y.high and tl = x.low + y.low
+       if th > maxval or tl < minval then z = TOP
+       else z.high = th and z.low = tl
+   - Note that we do not need to compare th < minval because 
+        if th < minval, then tl < minval.
+*/
+
+
 #define intbits ((sizeof(int))*8)
 
-#define mask(w) ((w) == sizeof(int)*8 ? 0xffffffff : (1 << (w)) - 1)
-#define minval(w) 0
-#define maxval(w) (-1 & mask(w))
-#define aint32(name) unsigned int name##low, name##high
+#define mask(w) ((w) == sizeof(long)*8 ? 0xffffffff : (1 << (w)) - 1)
 
-#define sign_extend(x,w) ((((int)(x)) << (intbits - (w))) >> (intbits - (w)))
+#define maxval(w) ((1l << (w-1)) - 1)
+#define minval(w) ((1l << (w-1)) * -1)
+#define aint(name,w) long name##low, name##high
+
+#define is_top(x) (x##high < x##low)
+
+//#define sign_extend(x,w) ((((int)(x)) << (intbits - (w))) >> (intbits - (w)))
 
 /* Set a variable to an interval. w is the word bith length  */
 #define set_interval(name,l,h,w) \
-  name##low = l & mask(w); \
-  name##high = h & mask(w)
+  name##low = l; \
+  name##high = h
 
 /* Adds x and y and stores into z. Assumes that 
    all abstract values have w bit width */
-#define add(z,x,y,w) \
-  z##high = (x##high + y##high) & mask(w);\
-  z##low = (x##low + y##low) & mask(w);\
-  if(x##low > x##high && z##high < x##high ||\
-     y##low > y##high && z##high < y##high ||\
-     x##low > x##high && y##low > y##high)\
-  {\
-     z##low = minval(w);                         \
-     z##high = maxval(w);\
-  }  
-
-/* Multiplies x and y and stores it in z. 
-   Must check how good this works for negative numbers. 
-   This abstracted instruction can be improved.*/
-#define mul(z,x,y,w)\
-  if(!(x##low == x##high && y##low == y##high) &&\
-    (x##high > maxval(w/2) || y##high > maxval(w/2) || \
-     x##low > x##high || y##low > y##high)) \
-  {\
-     z##low = minval(w);\
-     z##high = maxval(w);\
-  }\
+#define add(z,x,y,w)\
+  if(is_top(x) || is_top(y))\
+    {z##high = -777; z##low = 777;}  \
   else\
-  {\
-    z##high = (x##high * y##high) & mask(w);\
-    z##low = (x##low * y##low) & mask(w);\
-  }
+    {if(x##high + y##high > maxval(w) || x##low + y##low < minval(w))   \
+       {z##high = -777; z##low = 777;} \
+     else{\
+         z##high = x##high + y##high;  \
+         z##low = x##low + y##low; \
+       }\
+    }
+  
 
+
+/* Merges the abstract values x and y and saves the result
+   in z. Parameter w is the bit width. This macro assumes
+   that both values x and y have the same bitwith w. */
+//#define merge(z,x,y,w)                        \
 
 #define icmp_sgt(z,x,y,w)\
 if((sign_extend(x##high) >= sign_extend(x##low) &&\
@@ -86,10 +129,10 @@ if((sign_extend(x##high) >= sign_extend(x##low) &&\
 
 #define test_interval(desc,name,exp_low,exp_high)           \
   if(name##low != exp_low || name##high != exp_high){   \
-    printf("ERROR! Incorrect value for test '%s' Current: [%u,%u] " \
-           "Expected: [%u,%u]\n"\
-           "       Line %d, File: %s\n", desc, name##low, name##high,    \
-           exp_low, exp_high,  __LINE__, __FILE__); \
+    printf("ERROR! Incorrect value for test '%s' Current: [%ld,%ld] " \
+           "Expected: [%ld,%ld]\n"\
+           "       Line %d, File: %s\n", desc, name##low, name##high,  \
+           (long)exp_low, (long)exp_high,  __LINE__, __FILE__);               \
     exit(1);\
   }\
   else\
--- absdom/test_absint_interval.c
+++ absdom/test_absint_interval.c
@@ -1,15 +1,18 @@
 
 
+
+
 #include <stdio.h>
 #include "absint_interval.h"
 
 
 void test_add_instr()
 {
-  aint32(x);
-  aint32(y);
-  aint32(z);
-    
+  aint(x,32);
+  aint(y,32);
+  aint(z,32);
+  
+
   set_interval(x,3,100,32);
   set_interval(y,10,200,32);
   add(z,x,y,32);
@@ -29,117 +32,41 @@ void test_add_instr()
   set_interval(y,-50,-10,32);
   add(z,x,y,32);
   test_interval("add: lower and high from neg val (32-bit)",z,-80,-15);
-  
+    
   set_interval(x,30,100,8);
   set_interval(y,50,156,8);
   add(z,x,y,8);
-  test_interval("add: max val goes over max (8-bit)",z,80,0);
+  test_interval("add: max val goes over max (8-bit)",z,777,-777);
 
   set_interval(x,2,3,3);
   set_interval(y,1,2,3);
   add(z,x,y,3);
-  test_interval("add: 3-bits ord sum with ord (case 1)",z,3,5);
+  test_interval("add: 3-bits ord sum with ord (case 1)",z,777,-777);
 
-  set_interval(x,5,6,3);
-  set_interval(y,1,3,3);
+  set_interval(x,0,1,3);
+  set_interval(y,2,2,3);
   add(z,x,y,3);
-  test_interval("add: 3-bits ord sum with ord (case 2)",z,6,1);
+  test_interval("add: 3-bits ord sum with ord (case 2)",z,2,3);
 
-  set_interval(x,7,7,3);
-  set_interval(y,7,7,3);
+  set_interval(x,-3,1,3);
+  set_interval(y,-1,2,3);
   add(z,x,y,3);
-  test_interval("add: 3-bits ord sum with ord (case 3)",z,6,6);
+  test_interval("add: 3-bits ord sum with ord (case 3)",z,-4,3);
 
-  set_interval(x,0,7,3);
-  set_interval(y,7,7,3);
+  set_interval(x,-3,1,3);
+  set_interval(y,-2,2,3);
   add(z,x,y,3);
-  test_interval("add: 3-bits ord sum with ord (case 4)",z,7,6);
+  test_interval("add: 3-bits ord sum with ord (case 4)",z,777,-777);
 
-  set_interval(x,1,3,3);
-  set_interval(y,6,1,3);
-  add(z,x,y,3);
-  test_interval("add: 3-bits ord sum with swap (case 1)",z,7,4);
-
-  set_interval(x,7,7,3);
-  set_interval(y,6,1,3);
-  add(z,x,y,3);
-  test_interval("add: 3-bits ord sum with swap (case 2)",z,0,7);
-
-  set_interval(x,1,7,3);
-  set_interval(y,6,1,3);
-  add(z,x,y,3);
-  test_interval("add: 3-bits ord sum with swap (case 3)",z,0,7);
-
-  set_interval(x,3,6,3);
-  set_interval(y,6,1,3);
-  add(z,x,y,3);
-  test_interval("add: 3-bits ord sum with swap (case 4)",z,1,7);
-
-  set_interval(x,6,1,3);
-  set_interval(y,1,3,3);
-  add(z,x,y,3);
-  test_interval("add: 3-bits swap sum with ord (case 1)",z,7,4);
-
-  set_interval(x,6,1,3);
-  set_interval(y,1,7,3);
-  add(z,x,y,3);
-  test_interval("add: 3-bits swap sum with ord (case 2)",z,0,7);
-
-  set_interval(x,6,1,3);
-  set_interval(y,3,6,3);
-  add(z,x,y,3);
-  test_interval("add: 3-bits swap sum with ord (case 3)",z,1,7);
-
-  set_interval(x,2,1,3);
-  set_interval(y,3,2,3);
-  add(z,x,y,3);
-  test_interval("add: 3-bits swap sum with swap",z,0,7);
-
-  set_interval(x,-20,-10,32);
-  set_interval(y,20,100,32);
-  add(z,x,y,32);
-  test_interval("add: in and max vals go over max for 32 bit width",z,0,90);
-  
 }
-
-void test_mul_instr()
-{
-  aint32(x);
-  aint32(y);
-  aint32(z);
-
-  set_interval(x,20,40,32);
-  set_interval(y,10,100,32);
-  mul(z,x,y,32);
-  test_interval("mul: legal positive values",z,200,4000);  
-
-  set_interval(x,20,40,32);
-  set_interval(y,10,0xffffff,32);
-  mul(z,x,y,32);
-  test_interval("mul: too large values",z,0,0xffffffff);  
-
-  set_interval(x,20,40,32);
-  set_interval(y,-2,-2,32);
-  mul(z,x,y,32);
-  test_interval("mul: negative number with interval",z,0,0xffffffff);  
-
-  set_interval(x,40,40,32);
-  set_interval(y,-2,-2,32);
-  mul(z,x,y,32);
-  test_interval("mul: negative without interval",z,-80,-80);  
-}
-
-
 
 
 
 int main()
 {
-  ptver_startup_check();
+    ptver_startup_check();
  
-  test_add_instr();
-  test_mul_instr();
-
+    test_add_instr();
   return 0;
 }
 
