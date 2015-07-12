35d6cb63528608c52c2bc72ae78950863b21023f
David Broman <dbro@kth.se>
2015-07-12 20:52:40 +0200

    New version of kta.ml

--- src/kta.ml
+++ src/kta.ml
@@ -11,6 +11,9 @@ let top =
 let nothing_msg = top ^. us"One binary executable (ELF) file must be provided.\n" ^.
                          us"Use option -help to get help.\n"
 
+(* Message that is printed when help is requested *)
+let help = top ^. us"options:\n" 
+
 (* The header for the help menu *) 
 let header = top ^. us"options:\n"
 
@@ -18,12 +21,13 @@ let header = top ^. us"options:\n"
 type optypes =
   Op_tafile | Op_exhaustive | Op_func | Op_args | Op_help
       
+(* List of options. Defines behavior and help text. *)
 let options =
   [(Op_tafile,     Uargs.Str, us"-tafile",     us" <file>",
        us"Specify the timing analysis (.ta) file");
    (Op_exhaustive, Uargs.No, us"-exhaustive", us"",
        us"Perform exhaustive search of all program paths.");
-   (Op_func,       Uargs.No, us"-func",       us" <name>",
+   (Op_func,       Uargs.Str, us"-func",       us" <name>",
        us"Perform analysis on a specific named function. This option is ignored " ^.
        us"if the -tafile is used.");
    (Op_args,       Uargs.StrList, us"-args",  us" <values>",
@@ -38,22 +42,24 @@ let options =
 
 (* ---------------------------------------------------------------------*)
 let main =
-  match Uargs.parse Sys.argv options with
+  match Uargs.parse (Sys.argv |> Array.to_list |> List.tl) options with
     (* Print error message if the options were incorrect *)
   | None,msg -> uprint_endline msg
   | Some(ops,args),_ ->
     (
       (* Check if -help *)
       if List.mem_assoc Op_help ops then
-        uprint_endline (top ^. Uargs.optionstext options ^. us"\n")
+        uprint_endline (help ^. Uargs.optionstext options ^. us"\n")
 
-      (* Check if there is not ELF-file given *)
-      else if List.length args != 1 then
+      (* Check if there is no ELF-file given *)
+      else if List.length args < 1 then
         uprint_endline nothing_msg 
 
-      (* Check if we should use tafile *)   
-      else if List.mem_assoc Op_tafile ops then
-        uprint_endline (us"Use ta-file: " ^. (List.assoc Op_tafile ops |> List.hd))
+      (* Check if we should use a tafile *)   
+      else if List.mem_assoc Op_tafile ops then(
+        uprint_endline (us"Use ta-file: " ^. (List.assoc Op_tafile ops |> List.hd));
+        List.iter uprint_endline args        
+      )
 
       (* Check if direct function analysis *)
       else if List.mem_assoc Op_func ops then (
@@ -64,7 +70,7 @@ let main =
           
       (* Print help *)
       else 
-        uprint_endline (top ^. Uargs.optionstext options ^. us"\n")
+        List.iter uprint_endline args
     )
     
 
