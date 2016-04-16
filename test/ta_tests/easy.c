df769c37cd4f60da9aece94e20c7eb403dab9a63
David Broman <dbro@kth.se>
2016-04-16 10:41:47 +0200

    Updated easy ta test.

---
+++ test/ta_tests/easy.c
@@ -0,0 +1,32 @@
+
+
+
+#include "tpp.h"
+
+int g1 = 1;
+int g2 = 7;
+//int g3;
+
+// To avoid that timing program points are duplicated, we must mark the
+// function to not be inlined.
+int __attribute__ ((noinline)) foo()
+{
+  int u = g1 * g2;
+  int i;
+
+  TPP(1);
+
+  for(i=0; i<g1*g2; i++)
+    u += i * g2;
+  TPP(2);
+  return u;
+}
+
+int main()
+{
+
+  
+  foo();
+  
+  return 0;
+}
--- test/ta_tests/pt.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifndef __PT_H_
-#define __PT_H_
-
-extern void TPP(int) asm("llvm.pt_timing_program_point");
-
-#endif
