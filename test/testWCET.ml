d79f997875bbe71b67c61d1b7c977f41875d9646
Rodothea-Myrsini Tsoupidi <tsoupidi@kth.se>
2017-01-25 16:48:44 +0100

    Regression Test

---
+++ test/testWCET.ml
@@ -0,0 +1,9 @@
+
+open MipsCfg
+
+let main = []
+	(*let prog = MipsSys.get_program "demo/array_mul.c" |>  MipsUtils.add_branch_symbols in	
+	let result = MipsCfg.test prog (Ustring.to_utf8 (us"array_mul")) (["a0=[1,2]";"a1=[1,2]";"a2=[1,3]"], false) in
+*)
+
+
