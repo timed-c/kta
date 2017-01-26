b41486acf18237a3ee6893c51e35b0805639916a
Rodothea-Myrsini Tsoupidi <tsoupidi@kth.se>
2017-01-26 11:31:11 +0100

    Changed to Utest equality

--- test/testWCET.ml
+++ test/testWCET.ml
@@ -5,18 +5,19 @@ open MipsUtils
 open Ustring.Op
 open Printf
 open Str
+open Utest
        
 let tmpfile = "temp-file-090916-070704.tmp"
 
 let compile_file fname args optimize debug =
   (try
-    MipsSys.pic32_compile ["test/demo/array_mul.c"] false optimize tmpfile
-  with Sys_error e ->
-    e |> eprintf "Error %s");
+     MipsSys.pic32_compile ["test/demo/array_mul.c"] false optimize tmpfile
+   with Sys_error e ->
+     e |> eprintf "Error %s");
   let prog = MipsSys.get_program tmpfile |>  MipsUtils.add_branch_symbols in
   let (prog,cfgmap) = MipsCfg.make_cfgmap fname prog in
   let program_code = MipsCfg.pprint_ocaml_cps_from_cfgmap true fname cfgmap prog in
-  let stdout = MipsSys.wcet_compile "array_mul" false (Ustring.to_utf8 program_code) args in
+  let stdout = MipsSys.wcet_compile "array_mul" false program_code args in
   try
     let regex = Str.regexp "BCET:[^0-9]*\\([0-9]+\\)\\(.\\|\n\\)*WCET:[^0-9]*\\([0-9]+\\)" in
     let _ = Str.search_forward regex stdout 0 in
@@ -27,8 +28,13 @@ let compile_file fname args optimize debug =
      raise Not_found)
        
 let main =
+  Utest.init "WCET";
+  
   let bcet,wcet = compile_file "array_mul" ["a0=[1,2]";"a1=[1,4]";"a2=[1,3]"] false false in
-  printf "%d %d\n" bcet wcet;
+  Utest.test_int "array_mul.c: Non optimized." bcet 118;
+  Utest.test_int "array_mul.c: Non optimized." wcet 1687;
   let bcet,wcet = compile_file "array_mul" ["a0=[1,2]";"a1=[1,4]";"a2=[1,3]"] true false in
-  printf "%d %d\n" bcet wcet
+  Utest.test_int "array_mul.c: Optimized." bcet 36;
+  Utest.test_int "array_mul.c: Optimized." wcet 308;
+
 
